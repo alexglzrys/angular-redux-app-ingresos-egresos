7277c3928052b6c9aa2d1456d36543969e84c79b
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-08 21:34:37 -0600

    pantalla de registro - formulario reactivo

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    // Construir formulario reactivo
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  registrarUsuario() {
+    console.log(this.registerForm.invalid);
+    console.log(this.registerForm.value);
+  }
 }
