86a356d81e771ef465f3a5eee4037ffe7dd5fe87
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-12 17:12:29 -0600

    formulario reactivo - capturar ingresos y egresos

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ingreso-egreso',
@@ -8,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresoEgresoComponent implements OnInit {
 
-  constructor() { }
+  ingresoEgresoForm!: FormGroup;
+  type: string = 'ingreso';
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.ingresoEgresoForm = this.fb.group({
+      description: ['', [Validators.required, Validators.minLength(5)]],
+      amount: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  register() {
+    if (this.ingresoEgresoForm.valid) {
+      console.log(this.ingresoEgresoForm.value);
+    }
   }
 
 }
