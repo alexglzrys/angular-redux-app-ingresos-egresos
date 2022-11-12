d06c224f822f51a64b9506e502b5cf44b3015df7
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-11 18:05:16 -0600

    agregar SweetAlert2 para mensajes de error y notificación

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -27,10 +28,33 @@ export class LoginComponent implements OnInit {
   login() {
     if (this.loginForm.valid) {
       const {email, password} = this.loginForm.value;
+      Swal.fire({
+        title: 'Espere',
+        text: 'Validando credenciales de acceso',
+        timerProgressBar: true,
+        showConfirmButton: false,
+        didOpen: () => {
+          Swal.showLoading(Swal.getDenyButton())
+        }
+      });
       this.authService.loginUser(email, password).then(response => {
-        console.log(response)
+        console.log(response);
+        // Destruir cualquier instancia abierta de SweetAlert
+        Swal.close();
         this.router.navigate(['']);
-      }).catch(err => console.log(err));
+      }).catch(err => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/wrong-password":
+          case "auth/user-not-found":
+            Swal.fire({
+              title: 'Lo sentimos',
+              text: 'Las credenciales de acceso son incorrectas, verifiquelas nuevamente',
+              icon: 'error'
+            });
+            break;
+        }
+      });
     }
   }
 
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -22,20 +23,47 @@ export class RegisterComponent implements OnInit {
     this.registerForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.email, Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(3)]]
     });
   }
 
   registrarUsuario() {
     if (this.registerForm.valid) {
       const {name, email, password} = this.registerForm.value;
+      Swal.fire({
+        title: 'Espere',
+        text: 'Registrando nuevo usuario',
+        timerProgressBar: true,
+        showConfirmButton: false,
+        didOpen: () => {
+          Swal.showLoading(Swal.getDenyButton())
+        }
+      });
       // Llamar al servicio para registrar nuevo usuario en Firebase
       this.authService.createUser(name, email, password).then(user => {
         console.log(user);
-        // Cuando se crea un usuario, Firebase automáticamente lo logea. Dicha información se obtiene a través del objeto devuelto 
+         // Destruir cualquier instancia abierta de SweetAlert
+         Swal.close();
+        // Cuando se crea un usuario, Firebase automáticamente lo logea. Dicha información se obtiene a través del objeto devuelto
         // Navegar al dashboard
         this.router.navigate(['']);
-      }).catch(err => console.error(err));
+      }).catch(err => {
+        console.log(err);
+        let message = 'Se ha presentado un error desconocido'
+        switch (err.code) {
+          case "auth/invalid-email":
+            message = 'El correo electrónico no parece ser un dato válido';
+            break;
+          case "auth/weak-password":
+            message = 'La contraseña debe tener al menos 6 caracteres';
+            break;
+        }
+        Swal.fire({
+          title: 'Lo sentimos',
+          text: message,
+          icon: 'error'
+        });
+      });
     }
   }
 }
