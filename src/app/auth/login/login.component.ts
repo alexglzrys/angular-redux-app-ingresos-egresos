7661dcf8df733ea033805a25e79937327ffe0c0f
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-12 15:01:54 -0600

    dispatch - activar y desactivar el loading

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { Store } from '@ngrx/store';
+
+// Redux
+import { AppState } from '../../app.reducer';
+import { Subscription } from 'rxjs';
+import * as ui from '../../shared/ui.actions';
 
 @Component({
   selector: 'app-login',
@@ -10,25 +16,41 @@ import Swal from 'sweetalert2';
   styles: [
   ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   loginForm!: FormGroup;
+  uiSubscription!: Subscription;
+  isLoading: boolean = false;
 
+  // Redux: Inyectar el servicio de Store para hacer uso del patrón Redux
   constructor(private fb: FormBuilder,
               private authService: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    // Construir formulario reactivo
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
+
+    // Redux: Suscribirme a los cambios del ui y guardar una referencia de la suscripción para cancelarla al abandonar el componente
+    this.uiSubscription = this.store.select('ui').subscribe(ui => {
+      console.log('Subscripción al ui');
+      this.isLoading = ui.isLoading;
+    })
+  }
+
+  ngOnDestroy(): void {
+      // Redux: Cancelar la suscripción al estado de la ui de la aplicación
+      this.uiSubscription.unsubscribe();
   }
 
   login() {
     if (this.loginForm.valid) {
       const {email, password} = this.loginForm.value;
-      Swal.fire({
+      /*Swal.fire({
         title: 'Espere',
         text: 'Validando credenciales de acceso',
         timerProgressBar: true,
@@ -36,13 +58,23 @@ export class LoginComponent implements OnInit {
         didOpen: () => {
           Swal.showLoading(Swal.getDenyButton())
         }
-      });
+      });*/
+
+      // Redux: Despachar acción para cambiar el estado del loading de la aplicación
+      this.store.dispatch( ui.isLoading() );
+
       this.authService.loginUser(email, password).then(response => {
         console.log(response);
         // Destruir cualquier instancia abierta de SweetAlert
-        Swal.close();
+        // Swal.close();
+
+        // Redux: Despachar acción para cambiar el loading a falso
+        this.store.dispatch( ui.stopLoading() );
         this.router.navigate(['']);
       }).catch(err => {
+        // Redux: Despachar acción para cambiar el loading a falso
+        this.store.dispatch( ui.stopLoading() );
+
         switch (err.code) {
           case "auth/invalid-email":
           case "auth/wrong-password":
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import Swal from 'sweetalert2';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { Subscription } from 'rxjs';
+import * as ui from 'src/app/shared/ui.actions';
 
 @Component({
   selector: 'app-register',
@@ -10,13 +14,16 @@ import Swal from 'sweetalert2';
   styles: [
   ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
   registerForm!: FormGroup;
+  uiSubscription!: Subscription;
+  isLoading: boolean = false;
 
   constructor(private fb: FormBuilder,
               private authService: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private store: Store<AppState>) { }
 
   ngOnInit(): void {
     // Construir formulario reactivo
@@ -25,12 +32,18 @@ export class RegisterComponent implements OnInit {
       email: ['', [Validators.email, Validators.required]],
       password: ['', [Validators.required, Validators.minLength(3)]]
     });
+
+    this.uiSubscription = this.store.select('ui').subscribe(ui => this.isLoading = ui.isLoading);
+  }
+
+  ngOnDestroy(): void {
+      this.uiSubscription.unsubscribe();
   }
 
   registrarUsuario() {
     if (this.registerForm.valid) {
       const {name, email, password} = this.registerForm.value;
-      Swal.fire({
+      /*Swal.fire({
         title: 'Espere',
         text: 'Registrando nuevo usuario',
         timerProgressBar: true,
@@ -38,15 +51,21 @@ export class RegisterComponent implements OnInit {
         didOpen: () => {
           Swal.showLoading(Swal.getDenyButton())
         }
-      });
+      });*/
+
+      this.store.dispatch( ui.isLoading() );
       // Llamar al servicio para registrar nuevo usuario en Firebase
       this.authService.createUser(name, email, password).then(user => {
-         // Destruir cualquier instancia abierta de SweetAlert
-         Swal.close();
+        // Destruir cualquier instancia abierta de SweetAlert
+        // Swal.close();
+
+        this.store.dispatch( ui.stopLoading() );
         // Cuando se crea un usuario, Firebase automáticamente lo logea. Dicha información se obtiene a través del objeto devuelto
         // Navegar al dashboard
         this.router.navigate(['']);
       }).catch(err => {
+        this.store.dispatch( ui.stopLoading() );
+
         console.log(err);
         let message = 'Se ha presentado un error desconocido'
         switch (err.code) {
