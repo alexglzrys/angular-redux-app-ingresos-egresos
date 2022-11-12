9431bc98b5c0f268f6fef0757e85a5e874c4d5c6
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-11 18:26:34 -0600

    obtener información del usuario activo en Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-ingreso-egreso-app';
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    // Suscribirme para conocer el estado actual del usuario de Firebase en la aplicación
+    this.authService.initAuthListener();
+  }
+
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,6 +9,15 @@ export class AuthService {
 
   constructor(private auth: AngularFireAuth) { }
 
+  // Método para conocer el estado actual del usuario (si esta conectado, desconectado, su información, etc)
+  // Lo ideal es ejecutar este método en el componente raiz de la aplicación
+  initAuthListener() {
+    this.auth.authState.subscribe(fire_user  => {
+      console.log(fire_user)
+      console.log(fire_user?.email, fire_user?.uid);
+    })
+  }
+
   createUser(name: string, email: string, password: string): Promise<firebase.auth.UserCredential> {
     // Retornar la promesa del usuario creado mediante email y password
     return this.auth.createUserWithEmailAndPassword(email, password);
