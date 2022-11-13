a758844f9396bedcdbc5839ad8c275fb5e5ca839
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-13 11:58:10 -0600

    obtener el uid de los documentos almacenados en la colección de items

--- src/app/services/ingreso-egreso.service.ts
+++ src/app/services/ingreso-egreso.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 // Firebase
 import { AngularFirestore, DocumentReference } from '@angular/fire/compat/firestore';
 import firebase from 'firebase/compat/app';
+import { map } from 'rxjs/operators';
 
 import { IngresoEgreso } from '../models/IngresoEgreso';
 import { AuthService } from './auth.service';
@@ -29,8 +30,21 @@ export class IngresoEgresoService {
 
   // Recuperar mi colección de docuementos (items) referentes a los ingresos y egresos
   ingresosEgresosListener(uid: string) {
-    return this.firestore.collection(`${ uid }/ingreso-egreso/items`).valueChanges().subscribe(items => {
-      console.log(items)
-    })
+    // Solo obtenemos los valores guardados de los documentos (valueChanges)
+    /*return this.firestore.collection(`${ uid }/ingreso-egreso/items`)
+      .valueChanges()
+      .subscribe(items => {
+        console.log(items)
+      });*/
+
+    // Me interesa conocer toda la información de cada documento "metadata y data", en cualquier momento. (snapshot)
+    return this.firestore.collection(`${ uid }/ingreso-egreso/items`)
+      .snapshotChanges()
+      .pipe(
+        // Solo me interesa conocer el id del documento, y construir y devolver un nuevo objeto con la información que necesito
+        // El snapshot me retorna toda la metadata del documento (incluido el id del mismo), y la función data() me retorna el valor actual del documento
+        map(collections => collections.map(docRef => ({uid: docRef.payload.doc.id, ...docRef.payload.doc.data() as any})))
+      )
+      .subscribe(items => console.log(items));
   }
 }
