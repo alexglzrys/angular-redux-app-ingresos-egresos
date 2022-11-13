49fd6591bc8d656bcc959f177bc90b207d28aea8
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-13 14:33:32 -0600

    Dashboard - generar estadística básica

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { Subscription } from 'rxjs';
+import { IngresoEgreso } from '../../models/IngresoEgreso';
 
 @Component({
   selector: 'app-estadistica',
@@ -6,11 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class EstadisticaComponent implements OnInit {
+export class EstadisticaComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  montoTotalIngresos: number = 0;
+  montoTotalEgresos: number = 0;
+  ingresos: number = 0;
+  egresos: number = 0;
+  ingresoEgresoSubs!: Subscription;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    // Suscribirme al estado actual de los ingresos y egresos
+    this.ingresoEgresoSubs = this.store.select('ingresosEgresos').subscribe(({ items }) => this.generarEstadistica(items));
+  }
+
+  ngOnDestroy(): void {
+    this.ingresoEgresoSubs.unsubscribe();
+  }
+
+  generarEstadistica(listado: IngresoEgreso[]) {
+    // Genererar mi estadistica
+    listado.forEach(item => {
+      if (item.type === 'ingreso') {
+        ++this.ingresos;
+        this.montoTotalIngresos += item.amount;
+      } else {
+        ++this.egresos;
+        this.montoTotalEgresos += item.amount;
+      }
+    })
   }
 
 }
