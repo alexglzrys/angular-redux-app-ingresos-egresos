a76dfb136d27637cb2147cd3f59da2a6e4d5ddec
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-13 18:52:59 -0600

    colocar nombre del usuario activo en el sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,12 +11,23 @@ import { AuthService } from '../../services/auth.service';
   styles: [
   ]
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
+
+  username: string | undefined;
+  authSubs!: Subscription;
 
   constructor(private authService: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    // La primera vez se obtiene un usuario nulo, pues forestore aun no lo recupera
+    // Sería interesante usar un filtro para asegurarnos que siempre se obtiene el nombre del usuario
+    this.authSubs = this.store.select('auth').subscribe(({ user }) => this.username = user?.name );
+  }
+
+  ngOnDestroy(): void {
+      this.authSubs.unsubscribe();
   }
 
   logout() {
